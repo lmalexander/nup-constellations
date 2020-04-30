caecbbb716ff4b74b4b5a863a34d2c2065931fe6
lmalexander <elizabeth.alexander@northwestern.edu>
2020-04-29 19:23:23 -0500

    Updates

---
+++ precache-manifest.c61835be50055cc1e483bc3c14854e9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d66a723cd9ce69835ae898e1f0dbe32",
+    "url": "/nup-constellations/index.html"
+  },
+  {
+    "revision": "a6e77d08f4a6685a652a",
+    "url": "/nup-constellations/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d74b0c0ecedd25fdf40c",
+    "url": "/nup-constellations/static/js/2.14c55f60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nup-constellations/static/js/2.14c55f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e77d08f4a6685a652a",
+    "url": "/nup-constellations/static/js/main.b7b0dd9f.chunk.js"
+  },
+  {
+    "revision": "23c118caa4e0c55301f9",
+    "url": "/nup-constellations/static/js/runtime-main.d5aaf5b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nup-constellations/static/media/logo.5d5d9eef.svg"
+  }
+]);
